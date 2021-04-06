77c53d585e00c28ef32f66487004f02d0401eb9b
vikrant <vikrant.singh07@gmail.com>
2021-04-06 11:41:06 +0530

    This is my first commit

---
+++ MavenProject/src/main/java/test/com/MavenProject/App.java
@@ -0,0 +1,13 @@
+package test.com.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is my first project" );
+    }
+}
